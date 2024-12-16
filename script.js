b4c332ae94067f3271beb7993b5ab9c8795e13dc
Nikososos <nickywolf@live.nl>
2024-12-16 14:23:55 +0100

    Adds random generated computerchoice to the game

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
-console.log("hello");
+let computerChoice = Math.random();
+
+function getComputerChoice() {
+        if (computerChoice < 0.34) {
+            computerChoice = "rock";
+        } else if (computerChoice <= 0.67) {
+            computerChoice = "scissors";
+        } else 
+        { computerChoice = "paper";
+        }
+}
+
+getComputerChoice()
+
+let humanChoice = prompt("rock, paper or scissors?");
+
+function getHumanChoice() {
+        if (humanChoice != "rock" || "paper" || "scissors") {
+            return "Please enter a valid choice";
+        }
+}
+
+getHumanChoice()
+console.log(getHumanChoice())
